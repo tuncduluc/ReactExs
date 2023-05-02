45247fb8b6c23ed131ed9829583c237e88e1f66a
tuncduluc <tuncduluc@gmail.com>
2023-05-02 15:53:12 +0300

    Welcome message with default name

--- my-app/src/components/Welcome.js
+++ my-app/src/components/Welcome.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 class Welcome extends React.Component {
+    
   render() {
     return <p>Welcome, {this.props.name}!</p>;
   }
 }
+
+Welcome.defaultProps = {   //So it does not change the message. Cuz I defined the name as "Feras" in App.js.
+    name: 'Default'
+}
+    
+
 
 export default Welcome;
