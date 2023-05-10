a2899993aaf60ee7913d1bc04308d96d54766d74
tuncduluc <tuncduluc@gmail.com>
2023-05-10 14:47:50 +0300

    Props - Second prop exercise

--- my-app/src/components/Welcome.js
+++ my-app/src/components/Welcome.js
@@ -3,14 +3,17 @@ import React from 'react';
 class Welcome extends React.Component {
     
   render() {
-    return <p>Welcome, {this.props.name}!</p>;
-  }
+    return (<div><p>Welcome, {this.props.name}!</p><p>Your age is, {this.props.age}.</p></div>);
+
+  
+}
 }
 
 Welcome.defaultProps = {   //So it does not change the message. Cuz I defined the name as "Feras" in App.js.
-    name: 'Default'
-}
+    name: 'Default',
+    age: 23
+  }
     
 
 
-export default Welcome;
+export default Welcome
