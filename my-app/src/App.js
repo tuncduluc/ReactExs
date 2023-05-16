83b388378b953704237751a926e08b0144e79b2b
tuncduluc <tuncduluc@gmail.com>
2023-05-16 23:18:48 +0300

    Events - ClickCounter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import ClickCounter from './components/ClickCounter';
 import Counter from './components/Counter';
 import Hello from './components/Hello.js'
 import Message from './components/Message.js'
@@ -15,7 +16,7 @@ function App() {
       <Message/>
       <Welcome name="John" age={15}/>
       <Counter initialValue={0} incInterval={599} incAmount={1} />
-
+      <ClickCounter/>
     </div>
   );
 }
---
+++ my-app/src/components/ClickCounter.js
@@ -0,0 +1,28 @@
+import React, {dwqdqwdwqdwqdqwdwq} from 'react';
+
+class ClickCounter extends React.Component {
+    state = {
+        count: 0
+    }
+
+
+incButtonClick = () => {
+    this.setState(prevState => ({
+            count: prevState.count + 1
+    }));
+}
+
+render() {    
+    return (
+        <div>
+            <div>
+                Counter: {this.state.count}
+            </div>
+            <button onClick={this.incButtonClick}> Click here </button>
+            </div>
+        
+    )
+}
+}
+
+export default ClickCounter;
